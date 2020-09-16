7fbd63ce1c8e213d3807a9668d39d1c278d4c2a1
Jen-Chieh <jcs090218@gmail.com>
2020-09-16 15:19:00 +0800

    Add core.

---
+++ un-mini.el
@@ -0,0 +1,38 @@
+;;; un-mini.el --- Automatically close minibuffer after it loses focus  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-09-16 15:17:15
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Automatically close minibuffer after if loses focus.
+;; Keyword: minibuffer
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/un-mini
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Automatically close minibuffer after it loses focus.
+;;
+
+;;; Code:
+
+
+
+(provide 'un-mini)
+;;; un-mini.el ends here
